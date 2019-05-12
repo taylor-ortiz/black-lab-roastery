1b4fdb7bca43336502fe270b13ee0025cd4c9e21
Taylor Ortiz <taylor.ortiz@optiv.com>
2019-05-12 14:13:36 -0600

    committing other logic to repo for new objects, new layouts, etc

--- black-lab-roastery/src/lwc/coffeeList/coffeeList.js
+++ black-lab-roastery/src/lwc/coffeeList/coffeeList.js
@@ -1,6 +1,7 @@
 import { LightningElement, track, wire } from 'lwc';
 import { NavigationMixin } from 'lightning/navigation';
 import getAllShops from '@salesforce/apex/ctrlCoffeeDistributors.getAllShops';
+import searchShops from '@salesforce/apex/ctrlCoffeeDistributors.searchShops';
 import myResource from '@salesforce/resourceUrl/coffee_image';
 export default class coffeeList extends NavigationMixin(LightningElement) {
     @track shops;
@@ -18,9 +19,26 @@ export default class coffeeList extends NavigationMixin(LightningElement) {
         }
     }
 
+    @wire(searchShops, {searchTerm: '$searchTerm'})
+	loadShops(result) {
+		this.shops = result.data;
+	}
+
+	handleSearchTermChange(event) {
+		// Debouncing this method: do not update the reactive property as
+		// long as this function is being called within a delay of 300 ms.
+		// This is to avoid a very large number of Apex method calls.
+		window.clearTimeout(this.delayTimeout);
+		const searchTerm = event.target.value;
+		// eslint-disable-next-line @lwc/lwc/no-async-operation
+		this.delayTimeout = setTimeout(() => {
+			this.searchTerm = searchTerm;
+		}, 300);
+	}
+
     handleRecordNavigagtion(evt) {
         let shopId = evt.currentTarget.getAttribute("data-id");
-        // Navigate to bear record page
+        // Navigate to shop record page
 		this[NavigationMixin.Navigate]({
 			type: 'standard__recordPage',
 			attributes: {
@@ -33,6 +51,6 @@ export default class coffeeList extends NavigationMixin(LightningElement) {
 
     // Function to show other option on how to qualify with directives
     get hasShops() {
-        return typeof this.error !== 'undefined';
+        return this.shops < 1;
     }
 }
